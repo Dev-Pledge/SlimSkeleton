77593b4c25fd8dc3ce1e83abb0d94f61525961e5
John Saunders <john@yettimedia.co.uk>
2018-02-11 20:51:19 +0000

    db setup included

--- src/settings.php
+++ src/settings.php
@@ -1,35 +1,35 @@
 <?php
 return [
-    'settings' => [
-        'displayErrorDetails' => true, // set to false in production
-        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+	'settings' => [
+		'displayErrorDetails'    => true, // set to false in production
+		'addContentLengthHeader' => false, // Allow the web server to send the content-length header
 
-        // Renderer settings
-        'renderer' => [
-            'template_path' => __DIR__ . '/../templates/',
-        ],
+		// Renderer settings
+		'renderer'               => [
+			'template_path' => __DIR__ . '/../templates/',
+		],
 
-        // Monolog settings
-        'logger' => [
-            'name' => 'slim-app',
-            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
-            'level' => \Monolog\Logger::DEBUG,
-        ],
+		// Monolog settings
+		'logger'                 => [
+			'name'  => 'slim-app',
+			'path'  => isset( $_ENV['docker'] ) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+			'level' => \Monolog\Logger::DEBUG,
+		],
 
-        // MySQL settings
-        'database' => [
-            'dsn' => 'localhost',
-            'user' => 'root',
-            'pass' => '',
-        ],
+		// MySQL settings
+		'database'               => [
+			'dsn'  => 'mysql:dbname=testdb;host=mysql-db',
+			'user' => 'root',
+			'pass' => 'test_pass',
+		],
 
-        'security' => [
-            'jwt' => [
-                'algorithm' => 'SHA256',
-                'secret' => 'asdasd',
-                'ttl' => 3600,
-                'ttr' => 7200,
-            ],
-        ],
-    ],
+		'security' => [
+			'jwt' => [
+				'algorithm' => 'SHA256',
+				'secret'    => 'asdasd',
+				'ttl'       => 3600,
+				'ttr'       => 7200,
+			],
+		],
+	],
 ];
