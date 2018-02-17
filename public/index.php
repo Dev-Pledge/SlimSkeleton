5ad0a71dc46782250486ea70dadbfd064662723f
John Saunders <john@yettimedia.co.uk>
2018-02-17 21:47:02 +0000

    added errors

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php
-//die( 'jaden' . rand( 0, 98789 ) );
 if ( PHP_SAPI == 'cli-server' ) {
 	// To help the built-in PHP dev server, check if the request was actually for
 	// something which should probably be served as a static file
@@ -23,48 +22,34 @@ $error_handler = new Raven_ErrorHandler( $client );
 $error_handler->registerExceptionHandler();
 $error_handler->registerErrorHandler();
 $error_handler->registerShutdownFunction();
-try {
-	throw new \Exception( 'oops' );
-} catch ( \Exception $e ) {
-	echo '<br />Capture = ';
-	echo $client->captureException( $e );
-	echo '<br />';
-
-}
 
 /**
  * Instantiate the app
  */
 $settings = require __DIR__ . '/../src/settings.php';
+$app         = new \Slim\App( $settings );
 
-$app        = new \Slim\App( $settings );
-$start      = round( microtime( true ), 4 );
-$timeMarker = $start;
-$t          = function ( $tag = 'Tag' ) use ( $start, &$timeMarker ) {
-	$result     = ( round( microtime( true ) - $timeMarker, 4 ) );
-	$timeMarker = round( microtime( true ), 4 );
-	echo $tag . ' TIME ' . $result . ' <br />';
-};
+require __DIR__ . '/../src/errors.php';
+
 /**
  * Set up dependencies
  */
 require __DIR__ . '/../src/dependencies.php';
-$t( 'Dependancy' );
 
 /**
  * Register middleware
  */
 require __DIR__ . '/../src/middleware.php';
-$t( 'Middle' );
+
 /**
  * Register routes
  */
 require __DIR__ . '/../src/routes.php';
-$t( 'Routes' );
+
 /**
  * Run app
  */
 $app->run();
-$t( 'Run' );
 
-echo 'poo';
+
+
---
+++ src/errors.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @var \Slim\App $app
+ */
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+$container = $app->getContainer();
+
+$container['notFoundHandler'] = function ( $c ) {
+	return function ( Request $request, Response $response ) use ( $c ) {
+		$data              = new stdClass();
+		$data->errorStatus = '404';
+		$data->sentRequestBody = $request->getBody();
+		$data->sentRequestMethod = $request->getMethod();
+		$data->sentAttributes = $request->getAttributes();
+		$data->sentParams = $request->getParams();
+		return $response->withJson( $data )->withStatus( 404 );
+	};
+};
